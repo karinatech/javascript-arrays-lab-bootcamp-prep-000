3a57d81206c6c506f3e6b46c824a4086ca7174f7
Karyna Dominguez <karinaminty@gmail.com>
2019-01-23 17:04:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function myfunc(kittens){
-  kittens.push(Ralf)
+  kittens.push("Ralph");
+  return kittens;
 }
